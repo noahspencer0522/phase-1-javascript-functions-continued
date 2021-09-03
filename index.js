d9da80622a2e66563e3ac33446688a3a9f54985f
Noah Spencer <noahspencer0522@gmail.com>
2021-09-03 17:43:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function saturdayFun(activity = 'roller-skate'){
     return `This Saturday, I want to ${activity}!`
 }
-function mondayWork(activity = 'This Monday, I will work from home'){
-const fn = function (){
-    console.log ('go to the office')
-    return `${activity} + ${fn}`
+function mondayWork(activity = 'This Monday, I will go to the office.'){
+    return `This Monday, I will ${activity}`
+}
+    
 
-}
-}
+
+
 
       
     
