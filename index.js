c109ea993e3f607d06148873a1f8ddbfcf991a1a
Noah Spencer <noahspencer0522@gmail.com>
2021-09-03 15:52:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Your code here
+function saturdayFun()
+    {return `This Saturday, I want to roller-skate!`}
+{
+     function dog(myDog){return `This Saturday, I want to ${myDog}`}
+        dog(`bathe my dog!`)
+    }
+    
+
+    
+
