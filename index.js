d7977aef108e5f415334f68285fac093eecba693
Noah Spencer <noahspencer0522@gmail.com>
2021-09-07 16:12:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function saturdayFun(activity = 'roller-skate'){
     return `This Saturday, I want to ${activity}!`
 }
-function mondayWork(activity = 'This Monday, I will go to the office.'){
-    return `This Monday, I will ${activity}`
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(wrapAd = `* creates highlighted adjective`){
+    r
+
 }
     
 
